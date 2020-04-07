2996e275e8db5fa49771fc1ba54ec38c9d6e4a7b
sarahsekkat <sarah.sekkat26@gmail.com>
2020-04-07 15:04:59 +0200

    Création des classes Client, Personne et Proprietaire

---
+++ src/main/java/AppImmo/entities/Proprietaire.java
@@ -0,0 +1,55 @@
+package AppImmo.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Proprietaire extends Personne {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String telephonePro;
+	private List<BienImmo> biensImmo;
+	
+	public Proprietaire() {
+		super();
+	}
+
+	public Proprietaire(String telephonePro, List<BienImmo> biensImmo) {
+		super();
+		this.telephonePro = telephonePro;
+		this.biensImmo = biensImmo;
+	}
+
+	public String getTelephonePro() {
+		return telephonePro;
+	}
+
+	public void setTelephonePro(String telephonePro) {
+		this.telephonePro = telephonePro;
+	}
+
+	public List<BienImmo> getBiensImmo() {
+		return biensImmo;
+	}
+
+	public void setBiensImmo(List<BienImmo> biensImmo) {
+		this.biensImmo = biensImmo;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Proprietaire [telephonePro=" + telephonePro + "]";
+	}
+	
+	
+
+}
