e647a5bcbb3398e68eb5a84423c05104d74dcf55
DocKoneo <koneochester@gmail.com>
2020-04-07 11:17:07 +0200

    Création projet

---
+++ src/main/java/AppImmo/ProjetAppImmoBackApplication.java
@@ -0,0 +1,13 @@
+package AppImmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAppImmoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAppImmoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AppImmo/ProjetAppImmoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package AppImmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAppImmoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
