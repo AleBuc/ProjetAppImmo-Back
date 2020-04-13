2fb61d07a08efd1fdf6cbc6aa8ca20a7f85d5ac1
DocKoneo <koneochester@gmail.com>
2020-04-13 23:01:51 +0200

    Ajout de @EnableSwagger2

--- src/main/java/AppImmo/ProjetAppImmoBackApplication.java
+++ src/main/java/AppImmo/ProjetAppImmoBackApplication.java
@@ -7,17 +7,19 @@ import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ProjetAppImmoBackApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetAppImmoBackApplication.class, args);
-	}
 
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("AppImmo")).build();
 	}
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAppImmoBackApplication.class, args);
+	}
+
 }
